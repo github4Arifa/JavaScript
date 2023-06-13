fb677d42d55b96bff6fdd62b0387bb1a7730e495
github4Arifa <arifa.iftikhar2003@gmail.com>
2023-06-13 03:37:19 -0700

    var,left, const

---
+++ jsVariabiles.js
@@ -0,0 +1,65 @@
+//var (this can be changed, reassigned,re-declare)
+// example:
+
+var i=4;a=7;
+if(l>e)
+{
+    console.log("e is smaller");
+}
+else{
+   console.log("l is greater")
+}
+i=7;a=9;
+if(l>e)
+{
+        console.log("e is smaller");
+}
+else{
+    console.log("l is greater")
+}
+var i=67;a=19;
+if(l>e)
+{
+    console.log("e is smaller");
+}
+else{
+    console.log("l is greater")
+}
+
+
+
+//left (in this value can be re-assigned but can't be declared)
+//let e=6;l=80;
+if(l>e)
+{
+    console.log("e is smaller");
+}
+else{
+    console.log("l is greater")
+}
+i=23;l=13; //re-assigned and rechanged
+if(l>e)
+{
+    console.log("e is smaller");
+}
+else{
+    console.log("l is greater")
+}
+let e=13;l=8; // re-declaringing gives error cause its its not possilble
+if(l>e)
+{
+    console.log("e is smaller");
+}
+else{
+    console.log("l is greater")
+}
+
+
+
+
+
+//const  => (in this vlue can not be changed , re-assigned,re-declared)
+const z=100;
+if(z<101){
+    console.log(arifa); 
+}
